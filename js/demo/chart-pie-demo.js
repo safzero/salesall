2d769e4a4bf4bfd8f4eac1abf55175270f79a370
edusg83 <edusg83@gmail.com>
2022-11-01 13:55:30 +0100

    dashboard

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -1,3 +1,29 @@
+
+
+
+function getLabelCharts(){
+  let labelsChart = [];
+  axios.get(urlGoals,{headers})
+  .then((respuesta)=>{
+    labelsChart=respuesta.data.map(element=> labelsChart.push(element.name));    
+  })
+  return labelsChart;
+
+}
+
+function getTransactionsEur(){
+  let transactionsEur = [];
+
+  axios.get(urlGoals,{headers})
+  .then((respuesta)=>{
+    respuesta.data.forEach(element => {
+      transactionsEur.push(element.transactions.length);
+    })
+  })
+  
+  return transactionsEur;
+}
+
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
@@ -7,9 +33,9 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: getLabelCharts(),
     datasets: [{
-      data: [55, 30, 15],
+      data: getTransactionsEur(),
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
